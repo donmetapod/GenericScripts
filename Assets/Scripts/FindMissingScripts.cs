86eda1862a8a2ece8a85197edb6786a0edcc3c91
donmetapod <tyrantmx@gmail.com>
2022-09-08 13:36:51 -0500

    added a batch of generic scripts

--- Assets/Scripts/FindMissingScripts.cs
+++ Assets/Scripts/FindMissingScripts.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-public class FindMissingScripts : MonoBehaviour
+public static class FindMissingScripts
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Utilities/Find Missing Scripts in Project")]
+    static void FindMissingScriptsInProject()
     {
-        
+        string[] _prefabPaths = AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".prefab", System.StringComparison.OrdinalIgnoreCase)).ToArray();
+        int _missingScriptsInPrefabsCount = 0;
+        foreach (var path in _prefabPaths)
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            foreach (var component in prefab.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.Log($"Prefab found with missing script {path}", prefab);
+                    _missingScriptsInPrefabsCount++;
+                    break;
+                }
+            }
+        }
+        if (_missingScriptsInPrefabsCount == 0)
+        {
+            Debug.Log("No missing scripts found in prefabs");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [MenuItem("Utilities/Find Missing Scripts in Scene")]
+    static void FindMissingScriptsInScene()
     {
-        
+        int _missingScriptsInObjectsCount = 0;
+        foreach (var gameObject in GameObject.FindObjectsOfType<GameObject>(true))
+        {
+            foreach (var component in gameObject.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.Log($"{gameObject.name} is missing a script", gameObject);
+                    _missingScriptsInObjectsCount++;
+                    break;
+                }
+            }
+        }
+
+        if (_missingScriptsInObjectsCount == 0)
+        {
+            Debug.Log("No missing scripts in scene objects");
+        }
     }
 }
